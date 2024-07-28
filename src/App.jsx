2d0ed149f2aae7360e03334b489300211d7fa711
anjalikolhatkar <111507878+anjalikolhatkar@users.noreply.github.com>
2024-07-28 19:34:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Hero from './components/custom/Hero';
+import MyFooter from './components/ui/footer';
+
+
+function App() {
+  return (
+    <>
+     
+      <Hero />
+      <MyFooter />
+
+    </>
+  )
+}
+
+export default App
